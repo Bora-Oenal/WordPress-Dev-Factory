f3b9fdbe643532cef6cddfadcf7f039975c7f4d5
Borazius76 <borazius1976@gmail.com>
2023-06-14 19:42:05 -0500

    add stlye.css into header.php / wp_enqueue_scripts

--- wp-content/themes/wp-dev-factoy/functions.php
+++ wp-content/themes/wp-dev-factoy/functions.php
@@ -4,4 +4,13 @@
  * 
  * @package wp-dev-factory
  */
+
+
+function enqueue_styles(){
+
+wp_enqueue_style( 'bo-style-files' , get_stylesheet_uri() , [] , false , all  );
+}
+add_action( 'wp_enqueue_scripts' , 'enqueue_styles' );
+
+
 ?>

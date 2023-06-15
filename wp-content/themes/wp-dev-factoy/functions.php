10c683544ece95f4e300c0bd09d6a4b181d6f9c6
Borazius76 <borazius1976@gmail.com>
2023-06-14 23:58:37 -0500

    bind bootstap css & js file in function.php

--- wp-content/themes/wp-dev-factoy/functions.php
+++ wp-content/themes/wp-dev-factoy/functions.php
@@ -9,13 +9,17 @@
  * Wir binden alle css & js-files
  *
  */
+
+
 function enqueue_styles()
 {
-    wp_enqueue_style("bo-style-files", get_stylesheet_uri(), [] , false , all);
+    wp_enqueue_style("mainstyles", get_stylesheet_uri(), [] , false , all);
+    wp_enqueue_style("bootstrap-styles", get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css' , [] , false , 'all');
 }
 function enqueue_js_scripts()
 {
-    wp_enqueue_script("bo-js-files", get_template_directory_uri() . '/assets/js/script.js' , [] , false , true);
+    wp_enqueue_script("custom-js", get_template_directory_uri() . '/assets/js/script.js' , [] , false , true);
+    wp_enqueue_script("bootstrap-js", get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.js' , ['jquery'] , false , true);
 }
 add_action("wp_enqueue_scripts", "enqueue_styles");
 add_action("wp_enqueue_scripts", "enqueue_js_scripts");

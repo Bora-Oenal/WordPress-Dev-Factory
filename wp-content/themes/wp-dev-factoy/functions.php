5998794275df287ef82ab88bde3728981d35d570
Borazius76 <borazius1976@gmail.com>
2023-06-14 20:32:30 -0500

    some files

--- wp-content/themes/wp-dev-factoy/functions.php
+++ wp-content/themes/wp-dev-factoy/functions.php
@@ -1,16 +1,22 @@
 <?php
 /**
  * Theme functions - functions.php File
- * 
+ *
  * @package wp-dev-factory
  */
 
-
-function enqueue_styles(){
-
-wp_enqueue_style( 'bo-style-files' , get_stylesheet_uri() , [] , false , all  );
+/**
+ * Wir binden alle css & js-files
+ *
+ */
+function enqueue_styles()
+{
+    wp_enqueue_style("bo-style-files", get_stylesheet_uri(), [] , false , all);
 }
-add_action( 'wp_enqueue_scripts' , 'enqueue_styles' );
-
-
+function enqueue_js_scripts()
+{
+    wp_enqueue_script("bo-js-files", get_template_directory_uri() . '/assets/js/script.js' , [] , false , true);
+}
+add_action("wp_enqueue_scripts", "enqueue_styles");
+add_action("wp_enqueue_scripts", "enqueue_js_scripts");
 ?>

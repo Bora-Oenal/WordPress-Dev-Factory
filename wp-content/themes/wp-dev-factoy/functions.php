5bb5b9300842fab69d0feaeb50d658438b2f2161
Borazius76 <borazius1976@gmail.com>
2023-06-16 18:33:07 -0500

    style edit

--- wp-content/themes/wp-dev-factoy/functions.php
+++ wp-content/themes/wp-dev-factoy/functions.php
@@ -30,11 +30,11 @@
 function theme_enqueue_scripts() 
 {
     // register styles
-    wp_register_style( 'mainstyles', get_stylesheet_uri(), [] , false, 'all');
+    wp_register_style( 'mainstyles', get_stylesheet_uri(), [] , filemtime( get_stylesheet_directory() . '/style.css' ) , 'all');
     wp_register_style( 'bootstrap-styles', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.js', [] , false, 'all' );
    
     // register scripts
-    wp_register_script( 'custom-js', get_template_directory_uri() . '/assetts/js/script.js', [] , false, true);
+    wp_register_script( 'custom-js', get_template_directory_uri() . '/assets/js/script.js', [] , filemtime( get_template_directory() . '/assets/js/script.js' ) , true);
     wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'] , false, true );
    
     // styles in die Warteschlange setzen
@@ -42,7 +42,7 @@ function theme_enqueue_scripts()
     wp_enqueue_style( 'bootstrap-styles' );
 
     // scripts in die Warteschlange setzen
-    wp_enqueue_script( 'rap-jscustom-js' );
+    wp_enqueue_script( 'custom-js' );
     wp_enqueue_script( 'bootstrap-js' );
 }
 
